b493d4b1aec34ef0487f32de053eac25f768e16c
wangjie <wangjie_fourth@163.com>
2024-03-24 03:27:55 +0800

    add: 模版方法

---
+++ behavioral/src/main/java/org/github/fourth/desgin/pattern/behavioral/templateMethod/BackfillFieldFromFileJob.java
@@ -0,0 +1,66 @@
+package org.github.fourth.desgin.pattern.behavioral.templateMethod;
+
+
+import javax.annotation.Resource;
+import java.util.List;
+
+public class BackfillFieldFromFileJob {
+    @Resource
+    private FileManager fileManager;
+
+    @Resource
+    private FieldService fieldService;
+
+
+    public void execute(String[] args) {
+        // task 0: verify environment
+        verifyEnvironmet();
+
+        // task 1: parameter check
+        checkParameter();
+
+        // task 2: Read backfill data from data source
+        List<DataModel> result = fileManager.read();
+
+        // task 3: Statisc data metric (count, notnull)
+        recordMetric();
+
+        // task 4: write data into storage
+        fieldService.updateFiled();
+
+        // task 5: post-backfill work
+        postBackFill();
+    }
+
+    private void postBackFill() {
+    }
+
+    private void recordMetric() {
+    }
+
+    private void checkParameter() {
+    }
+
+
+    private void verifyEnvironmet() {
+    }
+
+    private static class FileManager {
+
+        public List<DataModel> read() {
+            return null;
+        }
+    }
+
+    private static class FieldService {
+
+        public void updateFiled() {
+
+        }
+    }
+
+    private static class DataModel {
+    }
+}
+
+
